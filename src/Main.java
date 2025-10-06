65694534dbe95c2eb167cb0ae3ef41a45669f3d6
aGith0bSzkola <githubszkola@gmail.com>
2025-10-06 15:17:51 +0200

    zadanie zadanie 2 i 3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int[] NewArray = new int[]{1,4,9,16,25,36,49,64,81,100};
-        for (int Sqrt : NewArray) {
-            System.out.println(Sqrt);
-            double Wyniki = Math.sqrt(Sqrt);
-            System.out.println(Wyniki);
+//        int[] NewArray = new int[]{1,4,9,16,25,36,49,64,81,100};
+//        for (int Sqrt : NewArray) {
+//            System.out.println(Sqrt);
+//            double Wyniki = Math.sqrt(Sqrt);
+//            System.out.println(Wyniki);
+//        }
+
+
+//        int[] FibonnaciArray = new int[]{};
+//        FibonnaciArray[0] = 0;
+//        FibonnaciArray[1] = 1;
+//        for (int i = 2; i < 40; i++) {
+//            FibonnaciArray[i] = FibonnaciArray[i-1] + FibonnaciArray[i +2];
+//            }
+//        for(int i = 0; i<FibonnaciArray.; i++){
+//            System.out.println(FibonnaciArray[i]);
+//        }
+            int[] stowka  = new int[100]{};
+        for (int i = 0; i < 100; i++) {
+            int randomNum = (int)(Math.random() * 101);
+             stowka.push(i);
         }
+
 
     }
 }
