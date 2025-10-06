356ef102d862379001fd2d4d8b95c352f47bd5ac
aGith0bSzkola <githubszkola@gmail.com>
2025-10-06 14:29:49 +0200

    zadanie pierwiastki

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] NewArray = new int[]{1,4,9,16,25,36,49,64,81,100};
+        for (int Sqrt : NewArray) {
+            System.out.println(Sqrt);
+            double Wyniki = Math.sqrt(Sqrt);
+            System.out.println(Wyniki);
+        }
+
     }
 }
